c5972d8e5a8871fcc2e04a3c2b29151b3dfe5383
ZackHazak <nihankinzaki@gmail.com>
2026-01-10 11:27:39 +0200

    Fix next-pwa config type error

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const withPWA = withPWAInit({
     dest: "public",
     disable: process.env.NODE_ENV === "development", // Disable PWA in dev
     register: true,
-    skipWaiting: true,
 });
 
 const nextConfig: NextConfig = {
